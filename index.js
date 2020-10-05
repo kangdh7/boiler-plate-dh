f38fa9695d3f848a2322a48305d07aa9ef442dc2
Kang <kangdongha@KangDongui-Mac-mini.local>
2020-10-05 22:48:43 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencode
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dhkang:1113@cluster0.vm67i.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -11,6 +22,21 @@ mongoose.connect('mongodb+srv://dhkang:1113@cluster0.vm67i.mongodb.net/<dbname>?
 
 app.get('/', (req, res) => {
   res.send('Hello World!~안녕하세요!')
+})
+
+app.post('/register', (req, res) => {
+  //회원 가입 필요 정보를 client 에서 가져오면 
+  //그걸 DB에 넣어준다.
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+      if (err) return res.json({ success: false, err })
+      return res.status(200).json({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {
